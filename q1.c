1eba8d15b4ef07d8e712c6a06ab4c6291dc3f74b
ashryaagrdev <56182664+ashryaagrdev@users.noreply.github.com>
2019-10-13 20:37:18 +0530

    q1c

---
+++ q1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int T = 0 ;
+    long long int N, K ;
+    scanf("%d", &T) ;
+    for(int i=0; i<T;i++){
+        scanf("%lld %lld", &N, &K) ;
+        printf("%lld")
+} 
